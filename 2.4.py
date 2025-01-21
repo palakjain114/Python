267828cbd3ab026dc06117b07e7a6be3269c2e1e
palakjain114 <palakjain1126@gmail.com>
2025-01-21 15:08:12 +0530

    Add files via upload

---
+++ 2.4.py
@@ -0,0 +1,8 @@
+def divisible():
+    n=int(input("Enter a number to check whether its divisible by 10 or not: "))
+    if (n%10==0):
+        print("The number is divisible by 10")
+    else:
+        print("The number is not divisible by 10")
+
+divisible()
